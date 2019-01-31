b9ecd9bc709dc628e55b595db60c0351863e433b
nicothebrush <nicola.riolini@gmail.com>
2019-01-31 09:42:15 +0100

    [ADD] sale_custom_name: add custom name in manual order

---
+++ sale_custom_name/README.md
@@ -0,0 +1,7 @@
+Product Pool Similar
+--------
+
+Manage linked product to generate 2 different pool:
+1. Similar pool: product are the same but with different code
+2. Alternative: product are not the same but have same caracteristic
+
---
+++ sale_custom_name/sale_custom.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2018 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import sys
+import openerp
+import logging
+from openerp import models, fields, api
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp.tools.translate import _
+from openerp.tools import (
+    DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare,
+    )
+
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrderLine(models.Model):
+    """ Model name: SaleOrderLine
+    """
+    
+    _inherit = 'sale.order.line'
+
+    # -------------------------------------------------------------------------
+    #                              ONCHANGE:
+    # -------------------------------------------------------------------------
+    # Override original action:    
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        ''' Create name with custom field
+        '''
+        res = super(SaleOrderLine, self).product_id_change()
+        if not self.product_id:
+            return res
+        if 'value' not in res:
+            res['value'] = {}
+        res['value']['name'] = self.product_id.mmac_product_name
+        return res
+        
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
