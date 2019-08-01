d926d2cfc83ffd090d3017acf22e9f2819059ae4
nicothebrush <nicola.riolini@gmail.com>
2019-08-01 17:54:18 +0200

    remove wizard add server action

--- tyres_logistic_pick_in_load/models/picking_load.py
+++ tyres_logistic_pick_in_load/models/picking_load.py
@@ -360,6 +360,66 @@ class PurchaseOrderLine(models.Model):
     #                                   Button event:
     # -------------------------------------------------------------------------
     @api.multi
+    def generate_delivery_orders_from_line(self):
+        ''' Create the list of all order received splitted for supplier        
+        '''
+        delivery_pool = self.env['stock.picking.delivery']
+
+        # ---------------------------------------------------------------------
+        # Extract supplier line list:
+        # ---------------------------------------------------------------------
+        suppliers = {} # TODO also with carrier?
+        for line in self:
+            supplier = line.order_id.partner_id
+            if supplier not in suppliers:
+                suppliers[supplier] = []
+            suppliers[supplier].append(line)            
+        
+        # ---------------------------------------------------------------------
+        # Create purchase order:
+        # ---------------------------------------------------------------------
+        delivery_ids = []
+        for supplier in suppliers:
+            # -----------------------------------------------------------------
+            # Create header:
+            # -----------------------------------------------------------------
+            delivery_id = delivery_pool.create({
+                'supplier_id': supplier.id,
+                #'carrier_id': carrier.id,
+                #'create_uid': self.env.uid,                
+                }).id
+            delivery_ids.append(delivery_id)
+            for line in suppliers[supplier]: # TODO better
+                line.delivery_id = delivery_id # Link to delivery order!
+                
+        # ---------------------------------------------------------------------
+        # Return created order:
+        # ---------------------------------------------------------------------
+        tree_view_id = form_view_id = False
+        if len(delivery_ids) == 1:
+            res_id = delivery_ids[0]
+            views = [(tree_view_id, 'form'), (tree_view_id, 'tree')]
+        else:
+            res_id = False    
+            views = [(tree_view_id, 'tree'), (tree_view_id, 'form')]
+            
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Delivery created:'),
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_id': res_id,
+            'res_model': 'stock.picking.delivery',
+            'view_id': tree_view_id,
+            #'search_view_id': search_view_id,
+            'views': views,
+            'domain': [('id', 'in', delivery_ids)],
+            'context': self.env.context,
+            'target': 'current', # 'new'
+            'nodestroy': False,
+            }
+
+    @api.multi
     def dummy(self):
         ''' Dummy button (do nothing)
         '''
