e49c3943a486193805e5e26c136ddb4297f49bd3
nicothebrush <nicola.riolini@gmail.com>
2019-10-01 18:30:22 +0200

    generic bugfix (fast message)

---
+++ tyres_logistic_order_unification/README.md
@@ -0,0 +1,6 @@
+Logistic Order Unification
+-----------------------
+
+Unificate different order comes in a defined period
+
+Fork of logistc_order_unification (not install both)
---
+++ tyres_logistic_order_unification/models/unification.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import sys
+import logging
+from odoo import fields, api, models
+from odoo import tools
+from odoo.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrderLine(models.Model):
+    ''' Sale order line update model
+    '''
+    _inherit = 'sale.order.line'
+
+    # -------------------------------------------------------------------------
+    # Columns:
+    # -------------------------------------------------------------------------
+    unification_origin_id = fields.Many2one(
+        'sale.order', 'Origin order', help='Order line comes from this order')
+
+class SaleOrder(models.Model):
+    ''' Sale order update model
+    '''
+    _inherit = 'sale.order'
+
+    @api.multi
+    def migrate_to_destination_button(self):
+        ''' Migrate button:
+        '''
+        self.ensure_one()
+        line_pool = self.env['sale.order.line']
+        
+        # Read linked destination (written before):
+        destination_id = self.order_destination_id.id
+        origin_id = self.id
+        
+        # Move line and keep origin reference:
+        lines = line_pool.search([
+            ('order_id', '=', origin_id)])
+            
+        lines.write({
+            'order_id': destination_id, # Change parent header
+            'unification_origin_id': origin_id,            
+            })    
+        return True
+        
+    # -------------------------------------------------------------------------
+    # Columns:
+    # -------------------------------------------------------------------------
+    order_destination_id = fields.Many2one(
+        'sale.order', 'Destination', help='Destination order for unification')
+    unificated_order_ids = fields.One2many(
+        'sale.order', 'order_destination_id', 'Uificated order', 
+        help='List of order unificated here')
+    unificated_line_ids = fields.One2many(
+        'sale.order.line', 'unification_origin_id', 'Unificated line', 
+        help='List of unificared line previous in order')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
