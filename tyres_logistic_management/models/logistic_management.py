797f5e6f14a716006c461af654fa7e0892f548c6
nicothebrush <nicola.riolini@gmail.com>
2019-10-02 14:22:32 +0200

    generic bugfix (fast message)

--- tyres_logistic_management/models/logistic_management.py
+++ tyres_logistic_management/models/logistic_management.py
@@ -1268,6 +1268,7 @@ class StockPicking(models.Model):
                         '', # comment
                         order.carrier_supplier_id.account_ref or '', # code
                         ))
+                import pdb; pdb.set_trace()        
 
                 # Invoice note:        
                 if order.note_invoice:
