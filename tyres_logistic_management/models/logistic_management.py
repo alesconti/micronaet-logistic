aeb36f2737c18176dc36efb39ba10d3ebdb1ca30
nicothebrush <nicola.riolini@gmail.com>
2019-10-08 15:53:20 +0200

    generic bugfix (fast message)

--- tyres_logistic_management/models/logistic_management.py
+++ tyres_logistic_management/models/logistic_management.py
@@ -833,7 +833,7 @@ class StockPicking(models.Model):
                 fees_f.write(
                     'CANALE|SKU|DATA|PAGAMENTO|PRODOTTO|Q|TOTALE|TIPO|'
                     'AGENTE\r\n')
-
+                import pdb; pdb.set_trace()
                 for row in channel_row[channel].values():
                     fees_f.write('%s|%s|%s|%s|%s|%s|%s|%s|%s\r\n' % row)
                 fees_f.close()
