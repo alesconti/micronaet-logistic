e14bf5fba32496afd2ae9317805abea505aac67b
nicothebrush <nicola.riolini@gmail.com>
2020-03-13 08:08:54 +0100

    add filter for print date

--- tyres_logistic_carrier/wizard/check_wizard.py
+++ tyres_logistic_carrier/wizard/check_wizard.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# ODOO (ex OpenERP) 
+# ODOO (ex OpenERP)
 # Open Source Management Solution
 # Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -13,7 +13,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
@@ -32,41 +32,55 @@ from odoo.tools.translate import _
 
 _logger = logging.getLogger(__name__)
 
+
 class SaleOrderCarrierCheckWizard(models.TransientModel):
     """ Model name: Order carrier check
     """
     _name = 'sale.order.carrier.check.wizard'
     _description = 'Carrier check wizard'
-    
+
     # -------------------------------------------------------------------------
     #                            COLUMNS:
-    # -------------------------------------------------------------------------    
+    # -------------------------------------------------------------------------
     carrier_id = fields.Many2one('carrier.supplier', 'Carrier Supplier')
     mode_id = fields.Many2one('carrier.supplier.mode', 'Mode')
     from_date = fields.Date('From date >=', required=True)
     to_date = fields.Date('To date <', required=True)
+    from_print_date = fields.Date('From print date >=')
+    to_print_date = fields.Date('To print date <')
     only_shippy = fields.Boolean('Shippy only', default=True)
-    # -------------------------------------------------------------------------    
+    # -------------------------------------------------------------------------
 
     @api.multi
     def extract_excel_report(self, ):
-        ''' Extract Excel report
-        '''
+        """ Extract Excel report
+        """
         line_pool = self.env['sale.order.line']
         excel_pool = self.env['excel.writer']
-        
+
         from_date = self.from_date
         to_date = self.to_date
+        from_print_date = self.from_print_date
+        to_print_date = self.to_print_date
         carrier = self.carrier_id
         mode = self.mode_id
         only_shippy = self.only_shippy
-        
+
         domain = [
             # Header
             ('order_id.date_order', '>=', from_date),
             ('order_id.date_order', '<', to_date),
-            ('order_id.logistic_state', '>=', 'done'), # only done order
+            ('order_id.logistic_state', '>=', 'done'),  # only done order
             ]
+
+        if from_print_date:
+            domain.append(
+                ('order_id.mmac_shippy_print_labels_date', '>=',
+                    from_print_date))
+        if to_print_date:
+            domain.append(
+                ('order_id.mmac_shippy_print_labels_date', '<',
+                    to_print_date))
 
         if carrier:
             domain.append(
@@ -89,20 +103,20 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
             'Modo', 'Data', 'Ordine', 'Destinazione',
             'Track ID', 'Shippy', 'Stampa',
             'Peso', 'Colli', 'Costo',
-            
+
             'Q.', 'Prodotto',
             )
         column_width = (
             16, 10, 20, 55,
             15, 5, 15,
             6, 6, 6,
-            
+
             6, 65,
             )
 
         # ---------------------------------------------------------------------
         # Write detail:
-        # ---------------------------------------------------------------------        
+        # ---------------------------------------------------------------------
         structure = {}
         lines = line_pool.search(domain)
         for line in lines:
@@ -111,17 +125,17 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
 
             order = line.order_id
             supplier = order.carrier_supplier_id
-            
+
             if supplier not in structure:
                 structure[supplier] = {}
             if order not in structure[supplier]:
                 structure[supplier][order] = []
-            structure[supplier][order].append(line)    
-        
+            structure[supplier][order].append(line)
+
         if not structure:
             _logger.warning('Order not found!')
             raise exceptions.Warning(_('No order found with this filters!'))
-            return True    
+            return True
 
         # -----------------------------------------------------------------
         #                   Excel sheet creation:
@@ -138,7 +152,7 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
             if not setup_complete: # First page only:
                 setup_complete = True
                 excel_pool.set_format()
-                format_text = {                
+                format_text = {
                     'title': excel_pool.get_format('title'),
                     'header': excel_pool.get_format('header'),
                     'white': {
@@ -148,12 +162,12 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
                     'red': {
                         'text': excel_pool.get_format('bg_normal_red'),
                         'number': excel_pool.get_format(
-                            'bg_normal_red_number'),                        
+                            'bg_normal_red_number'),
                         },
                     'green': {
                         'text': excel_pool.get_format('bg_green'),
                         'number': excel_pool.get_format(
-                            'bg_green_number'),                        
+                            'bg_green_number'),
                         },
                     }
 
@@ -164,31 +178,31 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
             excel_pool.write_xls_line(ws_name, row, [
                 u'Corriere: %s, Modo: %s, Data [%s, %s] %s' % (
                     carrier.name or 'Tutti',
-                    mode.name or 'Tutti',                    
+                    mode.name or 'Tutti',
                     from_date,
                     to_date,
                     'Solo Shippy' if only_shippy else '',
                     )
                 ], default_format=format_text['title'])
-                
+
             row += 2
-            excel_pool.write_xls_line(ws_name, row, header, 
+            excel_pool.write_xls_line(ws_name, row, header,
                 default_format=format_text['header'])
 
             total = {
-                'weight': 0.0,                
-                'parcel': 0.0,                
-                'cost': 0.0,                
+                'weight': 0.0,
+                'parcel': 0.0,
+                'cost': 0.0,
                 'qty': 0.0,
-                }            
-            for order in sorted(structure[supplier], 
+                }
+            for order in sorted(structure[supplier],
                     key=lambda x: (
                         x.carrier_mode_id.name or '',
                         x.date_order or '',
                         )):
-                row += 1        
-                partner = order.partner_shipping_id                
-                
+                row += 1
+                partner = order.partner_shipping_id
+
                 if order.carrier_shippy:
                     parcel = len(order.parcel_ids)
                     weight = sum([item.weight for item in order.parcel_ids])
@@ -197,15 +211,15 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
                     weight = order.carrier_manual_weight
 
                 # -------------------------------------------------------------
-                # Total:    
+                # Total:
                 # -------------------------------------------------------------
                 total['weight'] += weight
                 total['parcel'] += parcel
                 total['cost'] += order.carrier_cost
-                
+
                 if order.carrier_cost > 0.0:
                     format_color = format_text['white']
-                else:    
+                else:
                     format_color = format_text['red']
 
                 order_header = (
@@ -214,7 +228,7 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
                     order.date_order[:10],
                     order.name,
                     u'%s [%s - %s]' % (
-                        partner.name, 
+                        partner.name,
                         partner.city or '/',
                         partner.country_id.name or '/',
                         ),
@@ -226,19 +240,19 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
                     (parcel, format_color['number']),
                     (order.carrier_cost, format_color['number']),
                     )
-                excel_pool.write_xls_line(ws_name, row, order_header, 
+                excel_pool.write_xls_line(ws_name, row, order_header,
                     default_format=format_color['text'])
-                    
-                first = True    
+
+                first = True
                 col = len(order_header)
                 for line in structure[supplier][order]:
                     row += 1
-                    
+
                     # Readability:
                     product = line.product_id
-                    qty = line.product_uom_qty # Delivered qty                
+                    qty = line.product_uom_qty # Delivered qty
                     total['qty'] += qty
-                    
+
                     # ---------------------------------------------------------
                     # Write data line:
                     # ---------------------------------------------------------
@@ -247,14 +261,14 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
                         row -= 1
                     else:
                         excel_pool.write_xls_line(ws_name, row, (
-                            '', '', '', '', '', '', '', '', '', # XXX 
+                            '', '', '', '', '', '', '', '', '', # XXX
                             ), default_format=format_text['white']['text'])
-                        
-                    excel_pool.write_xls_line(ws_name, row, (                
+
+                    excel_pool.write_xls_line(ws_name, row, (
                             (qty, format_text['white']['number']),
                             product.name_extended,
-                        ), 
-                        default_format=format_text['white']['text'], 
+                        ),
+                        default_format=format_text['white']['text'],
                         col=col)
 
             # -----------------------------------------------------------------
@@ -263,13 +277,13 @@ class SaleOrderCarrierCheckWizard(models.TransientModel):
             # Total
             row += 1
             excel_pool.write_xls_line(ws_name, row, (
-                u'Totale:', 
+                u'Totale:',
                 total['weight'],
                 total['parcel'],
                 total['cost'],
                 total['qty'],
                 ), default_format=format_text['green']['number'], col=col - 4)
-                    
+
         _logger.warning('Supplier found: %s [Lines: %s]' % (
             len(structure),
             len(lines),
