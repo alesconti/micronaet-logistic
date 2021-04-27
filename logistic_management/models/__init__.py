5d6ac452aa0627e4f13f2aa49b9f93da341bb4f2
nicothebrush <nicola.riolini@gmail.com>
2021-04-27 15:00:29 +0200

    add dep

--- logistic_management/models/__init__.py
+++ logistic_management/models/__init__.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python
 ###############################################################################
 #
-# ODOO (ex OpenERP) 
+# ODOO (ex OpenERP)
 # Open Source Management Solution
 # Copyright (C) 2001-2018 Micronaet S.r.l. (<https://micronaet.com>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -13,7 +13,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
@@ -22,5 +22,6 @@
 ###############################################################################
 
 from . import logistic_management
-#from . import account_parameter # Parameter
+# from . import account_parameter # Parameter
 from . import dropshipping # Dropship management
+from . import api_management
