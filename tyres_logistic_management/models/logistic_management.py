fd85a36b67b9a9c466809b05850965e8c1039780
nicothebrush <nicola.riolini@gmail.com>
2021-06-10 09:52:52 +0200

    debug operation

--- tyres_logistic_management/models/logistic_management.py
+++ tyres_logistic_management/models/logistic_management.py
@@ -1340,6 +1340,7 @@ class StockPicking(models.Model):
         self.ensure_one()
 
         # Readability:
+        import pdb; pdb.set_trace()
         picking = self
         order = picking.sale_order_id
         partner = order.partner_invoice_id or order.partner_id
